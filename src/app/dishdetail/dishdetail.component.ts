ff8aed3f4593d9aaf514134586aceab24e471cfb
reptilcuantico <nicolasg99dr@gmail.com>
2022-05-21 19:19:01 -0300

    RxJS Part 2

--- src/app/dishdetail/dishdetail.component.ts
+++ src/app/dishdetail/dishdetail.component.ts
@@ -3,6 +3,7 @@ import { Dish } from '../shared/dish';
 import { Params, ActivatedRoute } from '@angular/router';
 import  { Location} from '@angular/common';
 import { DishService } from '../services/dish.service';
+import { switchMap } from 'rxjs';
 
 
 
@@ -21,6 +22,9 @@ export class DishdetailComponent implements OnInit {
     
 
     dish!: Dish;
+    dishIds!:string[];
+    prev!:string;
+    next!:string;
 
     constructor(private dishservice: DishService,
       private route: ActivatedRoute,
@@ -28,10 +32,18 @@ export class DishdetailComponent implements OnInit {
 
 
     ngOnInit() {
-      const id = this.route.snapshot.params['id'];
-      this.dishservice.getDish(id).subscribe(dish => this.dish = dish)
+      this.dishservice.getDishIds()
+      .subscribe((dishIds) =>this.dishIds = this.dishIds)
+      const id = this.route.snapshot.params['pipe'].switchMap((params:Params)=>this.dishservice.getDish(params['id']))
+      .subscribe((dish: Dish) => {this.dish = dish;this.setPrevNext(dish.Id);});
     }
   
+    //indexOf toma un valor y encuentra el índice de ese valor en la matriz.
+    setPrevNext(dishId: string){
+const index = this.dishIds.indexOf(dishId);
+this.prev = this.dishIds[(this.dishIds.length + index - 1) % this.dishIds.length]
+this.next = this.dishIds[(this.dishIds.length + index + 1) % this.dishIds.length]
+    }
     goBack(): void {
       this.location.back();
     }
